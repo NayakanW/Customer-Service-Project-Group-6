d6db0bada06c980c0a7ce35606fcd5fdcd6629f2
Muhamad Rafi Ramdhani <muhamadrafiramdhani15@gmail.com>
2025-04-18 14:18:11 +0700

    feat: Membuat route user dan employee

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
+Route::get('/employee', function () {
+    return view('employee.dashboard');
 });
+
+Route::get('/user', function () {
+    return view('user.dashboard');
+});
+
 
